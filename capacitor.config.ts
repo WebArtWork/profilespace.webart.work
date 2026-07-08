f4433611b0a17794048b7c84f8b529c8afbeec2c
OlefirenkoKristina <kristinaolefirenko5@gmail.com>
2026-07-08 13:37:07 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-	appId: 'com.example.appprofilespace.webart.work',
-	appName: 'Profile Space',
-	webDir: 'dist/app/browser',
-};
-
-export default config;
